4164dec6610747915b38e1a9651eef4e54a93bc8
shaik Asif <smd.20sa@gmail.com>
2024-03-03 23:28:59 +0530

    depricated

--- invoice-backend/config/db.js
+++ invoice-backend/config/db.js
@@ -6,7 +6,6 @@ dotenv.config();
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGODB_URI, {
-      useNewUrlParser: true,
       useUnifiedTopology: true,
     });
     console.log("MongoDB Connected");
